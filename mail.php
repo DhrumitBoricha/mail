60b9e2340dd2e596069780be54ec31ea30eba8cc
DhrumitBoricha <82996982+DhrumitBoricha@users.noreply.github.com>
2021-12-18 13:35:40 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $email= $_POST['emailaddress'];
 $message= $_POST['message'];
 $to = "dhrumitboricha1255@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
 $headers = "From: noreply@bdtech.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
